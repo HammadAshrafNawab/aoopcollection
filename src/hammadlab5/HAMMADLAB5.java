65d7e7ec7d33adb48515c4c2bc3084104c855672
student <student@CS-LAB3-203.numlit.edu.pk>
2020-10-21 17:39:37 +0500

    12

--- src/hammadlab5/HAMMADLAB5.java
+++ src/hammadlab5/HAMMADLAB5.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//COLLECTIONS
 package hammadlab5;
-
+import java.util.*;
 /**
  *
  * @author student
@@ -15,7 +16,88 @@ public class HAMMADLAB5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Mujtaba");
+        list.add("MURTAZA");
+        list.add("HAMMAD");
+        
+     // Iterator itr=list.iterator();
+       /* while(itr.hasNext())
+        {
+        System.out.println(itr.next());    
+        }
+        */
+     /*  for(String obj:list)
+       {
+       System.out.println(obj);
+       }
+       */
+    /*  ArrayList<String> list2 = new ArrayList<String>();
+      list2.add("GILO");
+        list2.add("SHAKO");
+        list2.add("DODA");
+        list.addAll(list2);
+          Iterator itr=list.iterator();
+        while(itr.hasNext())
+        {
+        System.out.println(itr.next());    
+        }
+      */
+    //list.removeAll(list2);
+    
+  /*  Collections.sort(list);
+      Iterator itr=list.iterator();
+        while(itr.hasNext())
+        {
+        System.out.println(itr.next());    
+        }
+*/
+ /* Collections.sort(list);
+  Collections.reverse(list);
+      Iterator itr=list.iterator();
+        while(itr.hasNext())
+        {
+        System.out.println(itr.next());    
+        }
+  */
+  /* Collections.shuffle(list);
+      Iterator itr=list.iterator();
+        while(itr.hasNext())
+        {
+        System.out.println(itr.next());    
+        }
+*/
+  /*Collections.fill(list,"ASHRAF");
+  System.out.println(list);
+*/
+ /* 
+  ArrayList<String> list2 = new ArrayList<String>();
+      list2.add("GILO");
+        list2.add("SHAKO");
+        list2.add("DODA");
+        Collections.copy(list2,list);
+          
+        
+        
+        System.out.println(list);    
+       
+  */
+/* int frequency = Collections.frequency(list,"HAMMAD");
+System.out.println(frequency);
+*/
+/*
+System.out.println(Collections.min(list));
+*/
+/* ArrayList<String> list2 = new ArrayList<String>();
+      list2.add("GILO");
+        list2.add("SHAKO");
+        list2.add("DODA");
+       Collections.disjoint(list, list2);
+        boolean disjoint = Collections.disjoint(list,list2); 
+        System.out.println(disjoint);
+*/
+
+}
     
 }
